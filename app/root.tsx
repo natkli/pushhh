15f17801c6b6a1cf2a3ea70e80bf1e44442a74a6
Nattaphong Klinjan <nattap1901@hotmail.com>
2024-06-03 17:42:37 +0200

    Add tailwind and shadcnui

--- app/root.tsx
+++ app/root.tsx
@@ -1,14 +1,19 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+import globalsStyles from "./globals.css?url";
+import { LinksFunction } from "@remix-run/node";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: globalsStyles },
+  {
+    rel: "icon",
+    type: "image/x-icon",
+    href: "favicon.ico",
+  },
+];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
